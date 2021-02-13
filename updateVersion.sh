61d97f5128b63562b86e6bee6d7b22a0e3540103
lihaifeng <li7hai26@gmail.com>
2021-02-14 01:42:08 +0800

    Sync package to central

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$0
-mvn deploy -Dmaven.test.skip=true -P release
-
+cd bsf-dependencies
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
+cd ../bsf-stater
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
+cd ..
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
