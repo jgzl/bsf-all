c769b302558663ee51e93b1c8afa591bc3b7f639
lihaifeng <li7hai26@gmail.com>
2021-02-14 10:10:46 +0800

    优化推送脚本

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 workdir=$(cd $(dirname $0); pwd)
 cd $workdir/bsf-dependencies && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
-cd $workdir/bsf-stater && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
+cd $workdir/bsf-starter && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
 cd $workdir && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 && mvn deploy -Dmaven.test.skip=true -P release
